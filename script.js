bc6c1b4f7babe375faa8a026121744615c876ead
IAMFALZIII <flop3z.11@gmail.com>
2022-06-21 19:49:22 -0700

    Added prompts for password generator

--- script.js
+++ script.js
@@ -12,12 +12,32 @@ function writePassword() {
   passwordText.value = password;
 
 }
+//* Created password characters
 function generatePassword() {
-  var lowercaseletters = "qwertyuiopasdfghjklzxcvbnm"
-  var uppercaseletters = "QWERTYUIOPASDFGHJKLZXCVBNM"
-  var numbers = "1234567890"
-  var specialchar = "~!@#$%^&*()-=+"
+  var lowercaseletters = "qwertyuiopasdfghjklzxcvbnm";
+  var uppercaseletters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+  var numbers = "1234567890";
+  var specialchar = "~!@#$%^&*()-=+";
+//* Ask for password info
+  var Length = prompt("Amount of characters between 8 to 128");
+  var Lowercase = prompt("Would you like lowercase characters?");
+  var Uppercase = prompt("Would you like uppercase characters?");
+  var Numbers = prompt("Would you like numbers?");
+  var Special = prompt("Would you like special characters?");
+
+  var password = "";
+  var selection = "";
+
+  userLength = parseInt(userLength);
+
+  if (userLowercase === true) {
+    selection = selection + lowercaseletters;
+
+  
+  }
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
